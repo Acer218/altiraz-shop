1cfcdecbd883eeccd84fa2f729dfffbb6e2c4831
Ace <hamedomanser@gmail.com>
2026-07-27 01:58:10 +0200

    Update Product.java

--- backend/Product.java
+++ backend/Product.java
@@ -9,11 +9,12 @@ public class Product {
     public List<String> images;
     public List<String> sizes;
     public boolean inStock;
+    public boolean featured;
 
     public Product(){
     }
 
-    public Product(int id, String name, String description, String category, double price, List<String> images, List<String> sizes, boolean inStock){
+    public Product(int id, String name, String description, String category, double price, List<String> images, List<String> sizes, boolean inStock, boolean featured){
         this.id = id;
         this.name = name;
         this.description = description;
@@ -22,5 +23,6 @@ public class Product {
         this.images = images;
         this.sizes = sizes;
         this.inStock = inStock;
+        this.featured = featured;
     }
 }
