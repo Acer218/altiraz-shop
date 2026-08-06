09cf4fcf917524c19026574e27210cabcf4cb0b2
Ace <hamedomanser@gmail.com>
2026-08-06 10:36:49 +0200

    Update SettingsDao.java

--- backend/SettingsDao.java
+++ backend/SettingsDao.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 public class SettingsDao {
 
     public Settings get() throws Exception {
-        String sql = "SELECT delivery_fee, inspection_fee FROM settings WHERE id=1";
+        String sql = "SELECT delivery_fee, inspection_fee, max_inspection_items FROM settings WHERE id=1";
         try(Connection con = Db.getConnection();
             PreparedStatement ps = con.prepareStatement(sql);
             ResultSet rs = ps.executeQuery()){
@@ -13,20 +13,23 @@ public class SettingsDao {
             if(rs.next()){
                 s.deliveryFee = rs.getDouble("delivery_fee");
                 s.inspectionFee = rs.getDouble("inspection_fee");
+                s.maxInspectionItems = rs.getInt("max_inspection_items");
             } else {
                 s.deliveryFee = 15;
                 s.inspectionFee = 20;
+                s.maxInspectionItems = 15;
             }
             return s;
         }
     }
 
     public void update(Settings s) throws Exception {
-        String sql = "UPDATE settings SET delivery_fee=?, inspection_fee=? WHERE id=1";
+        String sql = "UPDATE settings SET delivery_fee=?, inspection_fee=?, max_inspection_items=? WHERE id=1";
         try(Connection con = Db.getConnection();
             PreparedStatement ps = con.prepareStatement(sql)){
             ps.setDouble(1, s.deliveryFee);
             ps.setDouble(2, s.inspectionFee);
+            ps.setInt(3, s.maxInspectionItems);
             ps.executeUpdate();
         }
     }
