8cc338b24f1bed058e5a3f8a9793b793c360986e
Ace <hamedomanser@gmail.com>
2026-08-07 19:51:42 +0200

    Update Db.java

--- backend/Db.java
+++ backend/Db.java
@@ -4,8 +4,8 @@ import java.sql.SQLException;
 
 public class Db {
     private static final String URL = "jdbc:postgresql://aws-0-eu-west-1.pooler.supabase.com:5432/postgres?sslmode=require";
-    private static final String USER = "postgres.qobjnpvzomnnzmnxgnak";
-    private static final String PASSWORD = "i HATE MY LIFE218";
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
